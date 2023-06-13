61a911cc9258b97582e74c03174f924f51556fd3
Sacchi4649 <sanyadika@gmail.com>
2023-06-14 00:26:50 +0700

    modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
+<<<<<<< Updated upstream
+=======
+# import pymongo
+# import numpy
+import json
+>>>>>>> Stashed changes
 import machine_learning
 
 app = Flask(__name__)
@@ -8,10 +14,19 @@ app = Flask(__name__)
 @app.route('/machine-learning', methods=['POST'])
 def findAll():
     json_data = request.get_json()
-    keywords = json_data.get("keywords")
-    city = json_data.get("city")
+    # keywords = json_data.get("keywords")
+    # city = json_data.get("city")
+    # print(request.json["keywords"])
+    keywords = request.json["keywords"]
+    city = request.json["city"]
     ML = machine_learning.recommend_destinations(keywords, city)
+<<<<<<< Updated upstream
     return jsonify(ML)
+=======
+
+    return jsonify({'recommendation': ML})
+    # return jsonify(ML)
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
